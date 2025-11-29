265fc62e63a9277452fd502e95ba9845b080dde1
mohamedelgohary <mohamed.abdelazim.elgohary@gmail.com>
2025-11-30 00:18:07 +0200

    enhance ui code

--- app/src/main/java/com/elgohary/newsapptask/presentation/favorites/components/DismissBackground.kt
+++ app/src/main/java/com/elgohary/newsapptask/presentation/favorites/components/DismissBackground.kt
@@ -1,10 +1,7 @@
 package com.elgohary.newsapptask.presentation.favorites.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Icon
@@ -16,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.elgohary.newsapptask.R
 import com.elgohary.newsapptask.presentation.designsystem.Dimens
 
@@ -24,15 +22,15 @@ fun DismissBackground(dismissState: SwipeToDismissBoxState) {
     val isDeleteSide = dismissState.dismissDirection == SwipeToDismissBoxValue.StartToEnd
 
     Column (
+        verticalArrangement = Arrangement.Center,
         modifier = Modifier
-            .fillMaxHeight()
+            .fillMaxHeight().width(30.dp)
             .background(
                 if (isDeleteSide) MaterialTheme.colorScheme.errorContainer
                 else Color.Transparent
             )
             .padding(horizontal = Dimens.SpacingXL, vertical = Dimens.SpacingMD),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top
     ) {
         if (isDeleteSide) {
             Icon(
